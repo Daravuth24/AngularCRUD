f5fada51efa432c1e606cf7d07062f1ca9e6e87d
daravuth <chaukoldaravuth21@kit.edu.kh>
2023-02-02 03:36:37 +0700

    CRUD

---
+++ AngularCrud/src/app/dialog/dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.scss']
+})
+export class DialogComponent {
+
+  academicList = ["Year 1", "Year 2", "Year 3", "Year 4"]
+
+}
